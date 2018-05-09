b9a59e6e7e95e86a33e1c3511f55ffee6c46c115
mitrajysm <mitrajayusman31@gmail.com>
2018-05-09 16:50:01 -0700

    github

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class koneksi {
    private $server = "localhost";
-   private $username = "root";
+   private $username = "dev";
    private $password = "";
-    private $db = "utsmitra";
+    private $db = "id5161521_stok_barang";
 
     function getKoneksi() {
         return new mysqli($this->server, $this->username,$this->password,$this->db);
